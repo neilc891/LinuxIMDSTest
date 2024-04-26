31f02b96deb3e7f0d87e65eb45d9f9a36659ed91
neilc891 <46613340+neilc891@users.noreply.github.com>
2024-04-26 15:55:57 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,29 +1,16 @@
-import json
 import requests
-metadata_url="http://169.254.169.254/metadata/scheduledevents"
-header={'Metadata':'true'}
-query_params={'api-version':'2020-07-01'}
+import time
+ 
+metadata_url = "http://169.254.169.254/metadata/scheduledevents"
+header = {'Metadata': 'true'}
+query_params = {'api-version': '2020-07-01'}
+ 
 def get_scheduled_events():
-    resp=requests.get(metadata_url,headers=header,params=query_params)
-    data=resp.json()
+    resp = requests.get(metadata_url, headers=header, params=query_params)
+    data = resp.json()
     return data
-scheduled_events = get_scheduled_events()
-print(scheduled_events)
-import os
  
-from azure.core.exceptions import HttpResponseError
-from azure.identity import DefaultAzureCredential
-from azure.monitor.ingestion import LogsIngestionClient
- 
-endpoint = os.environ['https://migrationtestlinux-cn6u.westeurope-1.ingest.monitor.azure.com']
-credential = DefaultAzureCredential()
- 
-client = LogsIngestionClient(endpoint=endpoint, credential=credential, logging_enable=True)
- 
-rule_id = os.environ['d61c710d283f42dbba4500b589b609ea']
-body = scheduled_events
- 
-try:
-    client.upload(rule_id=rule_id, stream_name=os.environ['LOGS_DCR_STREAM_NAME'], logs=body)
-except HttpResponseError as e:
-    print(f"Upload failed: {e}")
+while True:
+    scheduled_events = get_scheduled_events()
+    print(scheduled_events)
+    time.sleep(10)  # Delay for 10 seconds
